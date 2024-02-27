c97b04de002c528d2c79bb5c2632c42f4a174ed4
benhurst1 <ben-hurst@outlook.com>
2024-02-27 18:35:42 +0000

    moved retrieve function into the route handler

--- app/api/map/route.js
+++ app/api/map/route.js
@@ -1,8 +1,17 @@
-import retrieveStations from "@/server/retrieveStations";
+const Station = require("../../../data/station");
+const mongoose = require("mongoose");
 
 export const dynamic = "force-dynamic"; // defaults to auto
 
 export async function GET() {
-  const stations = await retrieveStations();
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB", error);
+    return [];
+  }
+  const stations = await Station.find({});
+
   return Response.json(stations);
 }
